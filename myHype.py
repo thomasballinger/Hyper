2779df5b73f1a5189cdb9fcfb7b76611cfbb73ce
Thomas Ballinger <thomasballinger@gmail.com>
2012-11-08 15:25:59 -0500

    Moved downloading functionality to Downloader class

--- myHype.py
+++ myHype.py
@@ -5,9 +5,10 @@ import sys, json, time, threading, urlparse
 import requests
 from bs4 import BeautifulSoup
 
+EXTENSION_MAP = {'audio/mpeg' : 'mp3'}
+
 class Scraper(object):
     """Gets list of songs that could be downloaded"""
-    extension_map = {'audio/mpeg' : 'mp3'}
 
     def __init__(self, path='popular/'):
         self.get_songs(path)
@@ -22,6 +23,32 @@ class Scraper(object):
         results = BeautifulSoup(self.get_page(path))
         self.song_list = json.loads(results.find('script', id='displayList-data').get_text())['tracks']
         [track.__setitem__('rank', i + 1) for i, track in enumerate(self.song_list)]
+
+    def download(self, song_numbers):
+        [Downloader(self.song_list[selected], self.session).start() for selected in song_numbers]
+        Downloader.printer()
+
+class Downloader(threading.Thread):
+    tracker = {}
+
+    def __init__(self, song, session):
+        threading.Thread.__init__(self)
+        self.song, self.session = song, session
+        self.tracker[self.song['song']] = 0
+        self.daemon = True
+
+    def update(self, percent):
+        self.tracker[self.song['song']] = percent
+
+    def run(self):
+        try:
+            response, filename = self.get_song_file(self.song)
+        except requests.HTTPError:
+            print 'Sorry dude. ' + self.song['song'] + ' is not available.'
+            self.tracker[self.song['song']] = -1
+            print self.tracker
+        else:
+            self.save_file(response, filename, self.update)
 
     def request_song_url(self, song):
         host = 'http://hypem.com/serve/source/'
@@ -48,49 +75,22 @@ class Scraper(object):
     def save_file(self, resp, filename, updater=None):
         """Returns song file object, used as 'saver' """
         size = int(resp.headers['content-length'])
-        f = open(filename + '.' + self.extension_map[resp.headers['content-type']], 'w')
+        f = open(filename + '.' + EXTENSION_MAP[resp.headers['content-type']], 'w')
         bytes_read = 0
         while bytes_read < size:
             data = resp.raw.read(min(1024*64, size-bytes_read))
             bytes_read += len(data)
             f.write(data)
-            if updater:
-                updater(bytes_read/size)
-            else:
-                sys.stdout.write('%.f%%' % (bytes_read/size*100) + ' done\r')
-                sys.stdout.flush()
-        print
+            updater(bytes_read/size)
         f.close()
-
-class Downloader(threading.Thread):
-    tracker = {}
-
-    def __init__(self, filer, saver, song):
-        threading.Thread.__init__(self)
-        self.song = song
-        self.filer = filer
-        self.saver = saver
-        self.tracker[self.song['song']] = 0
-
-    def update(self, percent):
-        self.tracker[self.song['song']] = percent
-
-    def run(self):
-        try:
-            response, filename = self.filer(self.song)
-        except requests.HTTPError:
-            print 'Sorry dude. ' + self.song['song'] + ' is not available.'
-            self.tracker[self.song['song']] = -1
-            print self.tracker
-        else:
-            self.saver(response, filename, self.update)
 
     @classmethod
     def printer(kls):
         while any([abs(v) != 1 for v in kls.tracker.values()]):
             for k,v in kls.tracker.items():
                 if abs(v) != 1:
-                    print k + ': ' + '%.f%%' % (v*100) + ' done\r'
+                    sys.stdout.write( ', '.join(['%s: %.f%% done' % (k, v*100) for k, v in kls.tracker.items() if v != -1]) + '\r')
+                    sys.stdout.flush()
                     time.sleep(.1)
 
 def CLI():
@@ -105,18 +105,7 @@ def CLI():
 
     selections = raw_input('What numbers would you like to download? ')
     parsed = [int(x.strip())-1 for x in selections.split(',')]
-
-    # for selected in parsed:
-    #     print 'Downloading ' + scraper.song_list[selected]['artist'] + ' - ' +scraper.song_list[selected]['song']
-    #     scraper.save_file(*scraper.get_song_file(scraper.song_list[selected]))
-
-    threads = []
-    for selected in parsed:
-        newth = Downloader(scraper.get_song_file, scraper.save_file, scraper.song_list[selected])
-        threads.append(newth)
-        newth.start()
-
-    Downloader.printer()
+    scraper.download(parsed)
 
 if __name__ == '__main__':
     CLI()
